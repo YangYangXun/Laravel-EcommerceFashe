d76f18854b333c4e1beb0d919df869c68a94d682
YangYangXun <asdf1995905@gmail.com>
2018-09-18 16:37:35 +0800

    Category table rename

--- database/seeds/CategoryTableSeeder.php
+++ database/seeds/CategoryTableSeeder.php
@@ -4,7 +4,7 @@ use App\Category;
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
-class CategoriesTableSeeder extends Seeder
+class CategoryTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->call(CategoriesTableSeeder::class);
+        $this->call(CategoryTableSeeder::class);
         $this->call(ProductsTableSeeder::class);
     }
 }
